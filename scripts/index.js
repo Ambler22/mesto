bc96f57c78eb4f54090fab3949920d364e30a804
Roman Drozdov <hamilion.91@mail.ru>
2021-04-25 23:55:55 +0300

    исправления

--- scripts/index.js
+++ scripts/index.js
@@ -7,30 +7,32 @@ let jobInput = document.querySelector('[name="description"]');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__jod');
 
-function togglePopup(event) {
+openPopupButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
+
+function openPopup(event) {
     event.preventDefault();
-    popup.classList.toggle('popup__is-opened');
+    popup.classList.add('popup_is-opened');
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
-    }
-
-openPopupButton.addEventListener('click', togglePopup);
-
-closePopupButton.addEventListener('click', togglePopup);
-
-function handleOverlayClick (event) {
-    if (event.target === event.currentTarget) {
-        togglePopup(event);
-    }
 }
 
-popup.addEventListener('click', handleOverlayClick);
+function closePopup(event) {
+    event.preventDefault();
+    popup.classList.remove('popup_is-opened');
+}
+
+
+//function handleOverlayClick (event) {
+//    if (event.target === event.currentTarget) {
+//        togglePopup(event);
+//    }
+//}
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    popup.classList.remove('popup__is-opened');
+    popup.classList.remove('popup_is-opened');
 }
-
-formElement.addEventListener('submit', formSubmitHandler);
